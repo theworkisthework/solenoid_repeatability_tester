c33d5d59cc11f8e6ddd8c9c78f450b631cc4b20e
Edward Ward <edward_ward@outlook.com>
2024-06-13 21:03:34 +0100

    chatGPT example

--- src/main.cpp
+++ src/main.cpp
@@ -1,167 +1,78 @@
-#include <Arduino.h>
-#include <SPI.h>
 #include <SD.h>
+#include <SPI.h>
 
-File sdCardFile;
+const int solenoidPin = 27;  // Pin connected to solenoid
+const int endstopPin = 34;   // Pin connected to endstop (must be interrupt-capable pin)
+const int chipSelect = 5;    // SD card CS pin
 
-// create a variable to store the time when the last reading was taken
-unsigned long lastTime = 0;
+volatile int endstopCounter = 0;
+unsigned long lastTrigger = 0;
+unsigned int lineNumber = 1; // Initialize line number
 
-// variable to store the count of the number of times the solenoid has been triggered
-int triggerCount = 0;
+const unsigned long maxOnDuration = 2000; // Maximum duration solenoid can stay on in milliseconds
+const unsigned long maxInterval = 3000;   // Maximum interval between cycles in milliseconds
 
-int solenoidPin = 27; // Solenoid pin
-int endStopPin = 34;  // End stop pin
+void endstopISR() {
+  endstopCounter++;  // Increment the counter in the ISR
+}
 
-int stopTestPin = 0; // Stop test pin
-
-// Triger is how often we can trigger the solenoid
-int minTriggerDelay = 250;  // Minimum delay between triggers
-int maxTriggerDelay = 1000; // Maximum delay between triggers
-
-int solenoidHoldTime = 500; // How long we hold the solenoid for
-
-// Sense time is how long we wait for the end stop to be triggered (more than maxSenseTime and we assume it's a failed activation)
-int minSenseTime = 100; // Minimum time between activating the solenoid and reading the end stop
-int maxSenseTime = 500; // Maximum time between activating the solenoid and reading the end stop
-
-// Consequitive triggers is the number of times we can trigger the solenoid in a row
-int consequitiveTriggers = 0;
-int passes = 0;
-int fails = 0;
-
-void writeToSD(String data)
-{
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  sdCardFile = SD.open("/test.txt", FILE_APPEND);
-
-  // if the file opened okay, write to it:
-  if (sdCardFile)
-  {
-    // write to the file
-    sdCardFile.println(data);
-    // close the file:
-    sdCardFile.close();
-  }
-  else
-  {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+void logResult(String result) {
+  File dataFile = SD.open("/text.txt", FILE_APPEND);
+  if (dataFile) {
+    dataFile.print(lineNumber);  // Print the line number
+    dataFile.print(": ");
+    dataFile.println(result);
+    dataFile.close();
+    Serial.print(lineNumber);    // Print the line number to serial monitor
+    Serial.print(": ");
+    Serial.println(result);
+    lineNumber++;  // Increment the line number for next entry
+  } else {
+    Serial.println("Error opening datalog.txt");
   }
 }
 
-void setup()
-{
-  // Open serial communications and wait for port to open:
+void setup() {
   Serial.begin(9600);
-  Serial.print("Initializing SD card...");
-
-  if (!SD.begin(5))
-  {
-    Serial.println("initialization failed!");
-    while (1)
-      ;
-  }
-  Serial.println("initialization done.");
-
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  // sdCardFile = SD.open("/test.txt", FILE_WRITE);
-
-  String data = "======= Starting Test =======";
-  writeToSD(data);
-  Serial.println(data);
-
-  // initalize the lastTime variable
-  lastTime = millis();
-
-  // initialize the solenoid pin as an output:
   pinMode(solenoidPin, OUTPUT);
-  // set the solenoid pin to LOW:
-  digitalWrite(solenoidPin, LOW);
-
-  // initialize the end stop pin as an input:
-  pinMode(endStopPin, INPUT);
+  pinMode(endstopPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(endstopPin), endstopISR, FALLING);  // Attach interrupt
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    while (1);
+  }
+  Serial.println("Card initialized.");
+  randomSeed(analogRead(0)); // Seed the random generator with a noise reading
 }
 
-void loop()
-{
-  String data = "";
-  if (digitalRead(stopTestPin) == LOW)
-  {
-    Serial.println("Stopping Test");
+void loop() {
+  unsigned long currentMillis = millis();
+  unsigned long randomInterval = random(maxInterval); // Generate a random interval
 
-    data = "======= End Test =======";
-    writeToSD(data);
-    Serial.println(data);
-    data = "===" + String(passes) + " PASSED, " + String(fails) + " FAILED, " + String(triggerCount) + " TOTAL ===\n";
-    writeToSD(data);
-    Serial.println(data);
-    while (1)
-      ;
-  }
+  if (currentMillis - lastTrigger >= randomInterval) {
+    lastTrigger = currentMillis;
+    endstopCounter = 0;  // Reset the endstop counter
 
-  // To avoid false positives, we need to ensure the endstop state goes from low (led on) to high (led off)
-  // To acheive that, we can check the endstop is in the expected state. It should start low (solenoid off)
-  // and then read high when the solendoid is triggered.
-
-  boolean endStopTriggered = false;
-  boolean endStopStartState = true;
-
-  if (digitalRead(endStopPin) == LOW)
-  {
-    endStopStartState = true;
-    // Trigger the solenoid
-    Serial.println("Triggering Solenoid");
-    // Trigger the solenoid
+    unsigned long onDuration = random(maxOnDuration); // Generate a random on duration
+    Serial.println("Triggering solenoid ON for " + String(onDuration) + " milliseconds");
     digitalWrite(solenoidPin, HIGH);
-    // Wait minSenseTime
-    delay(minSenseTime);
-    // update lastTime
-    lastTime = millis();
-  }
+    delay(onDuration);
+    digitalWrite(solenoidPin, LOW);
+    Serial.println("Solenoid OFF");
 
-  // Keep checking the end stop until it's triggered or maxSenseTime is reached, if it's triggered, set endStopTriggered to true
-  while (millis() - lastTime < maxSenseTime)
-  {
-    if (digitalRead(endStopPin) == HIGH && endStopStartState)
-    {
-      endStopTriggered = true;
-      break;
+    unsigned long startTime = millis();
+    while (millis() - startTime < 500) { // 500 ms timeout for detecting endstop
+      if (endstopCounter >= 2) { // Check if both pulses are detected
+        break;
+      }
     }
+
+    if (endstopCounter >= 2) {
+      logResult("Success");
+    } else {
+      logResult("Failed");
+    }
+
+    Serial.println("Cycle completed. Waiting for next trigger.");
   }
-  // If the end stop was triggered, write a pass result to the sd card and serial else write a fail result to the sd card and serial
-  if (endStopTriggered && endStopStartState)
-  {
-    // reset endstop start state
-    endStopStartState = false;
-    // increnent consequitiveTriggers
-    consequitiveTriggers++;
-    passes++;
-    // Create a test result string
-    data = String(triggerCount) + ": PASSED in " + (millis() - lastTime) + "ms. Consequitive Triggers: " + String(consequitiveTriggers);
-  }
-  else
-  {
-    // Create a test result string
-    data = String(triggerCount) + ": FAILED in " + (millis() - lastTime) + "ms. Fail after " + String(consequitiveTriggers) + " Consequitive Triggers";
-    // Reset consequitiveTriggers after a fail
-    consequitiveTriggers = 0;
-    fails++;
-  }
-  // Increment the trigger count
-  triggerCount++;
-  // wait until hold time is reached then reset the solenoid
-  delay(solenoidHoldTime);
-  // reset the solenoid
-  digitalWrite(solenoidPin, LOW);
-  // reset endStopTriggered
-  endStopTriggered = false;
-  // Wait a random amount of time between minTriggerDelay and maxTriggerDelay
-  delay(random(minTriggerDelay, maxTriggerDelay));
-  // Write the test data to the sd card
-  writeToSD(data);
-  // Write the test data to serial
-  Serial.println(data);
 }
