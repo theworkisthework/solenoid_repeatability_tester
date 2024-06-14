204b80aa35f034198385672d340db9d04f675cb5
Mark Benson <mark@sub-design.co.uk>
2024-06-14 20:27:26 +0100

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <SD.h>
 #include <SPI.h>
 
-const int solenoidPin = 27;  // Pin connected to solenoid
-const int endstopPin = 34;   // Pin connected to endstop (must be interrupt-capable pin)
-const int chipSelect = 5;    // SD card CS pin
+const int solenoidPin = 27; // Pin connected to solenoid
+const int endstopPin = 34;  // Pin connected to endstop (must be interrupt-capable pin)
+const int chipSelect = 5;   // SD card CS pin
 
 volatile int endstopCounter = 0;
 unsigned long lastTrigger = 0;
@@ -12,34 +12,42 @@ unsigned int lineNumber = 1; // Initialize line number
 const unsigned long maxOnDuration = 2000; // Maximum duration solenoid can stay on in milliseconds
 const unsigned long maxInterval = 3000;   // Maximum interval between cycles in milliseconds
 
-void endstopISR() {
-  endstopCounter++;  // Increment the counter in the ISR
+void endstopISR()
+{
+  endstopCounter++; // Increment the counter in the ISR
 }
 
-void logResult(String result) {
+void logResult(String result)
+{
   File dataFile = SD.open("/text.txt", FILE_APPEND);
-  if (dataFile) {
-    dataFile.print(lineNumber);  // Print the line number
+  if (dataFile)
+  {
+    dataFile.print(lineNumber); // Print the line number
     dataFile.print(": ");
     dataFile.println(result);
     dataFile.close();
-    Serial.print(lineNumber);    // Print the line number to serial monitor
+    Serial.print(lineNumber); // Print the line number to serial monitor
     Serial.print(": ");
     Serial.println(result);
-    lineNumber++;  // Increment the line number for next entry
-  } else {
+    lineNumber++; // Increment the line number for next entry
+  }
+  else
+  {
     Serial.println("Error opening datalog.txt");
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(solenoidPin, OUTPUT);
   pinMode(endstopPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(endstopPin), endstopISR, FALLING);  // Attach interrupt
-  if (!SD.begin(chipSelect)) {
+  attachInterrupt(digitalPinToInterrupt(endstopPin), endstopISR, FALLING); // Attach interrupt
+  if (!SD.begin(chipSelect))
+  {
     Serial.println("Card failed, or not present");
-    while (1);
+    while (1)
+      ;
   }
   Serial.println("Card initialized.");
   randomSeed(analogRead(0)); // Seed the random generator with a noise reading
@@ -73,6 +81,6 @@ void loop() {
       logResult("Failed");
     }
 
-    Serial.println("Cycle completed. Waiting for next trigger.");
+    Serial.println("Cycle completed. Waiting for next trigger after " + String(millis() - currentMillis) + " milliseconds.");
   }
 }
